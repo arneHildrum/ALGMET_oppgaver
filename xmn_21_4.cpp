3c76d276beb43e0d4a761d37b6c6d66b2b3e3892
arnehi <arne.hildrum@ntnu.no>
2024-12-07 17:25:31 +0100

    code: Changed main, but still errors

--- xmn_21_4.cpp
+++ xmn_21_4.cpp
@@ -4,6 +4,9 @@
 
 using namespace std;
 
+const int ARRAY1LENGTH = 15;
+const int ARRAY2LENGTH = 6;
+
 void sortTwoArrays(int a[], const int b[], const int aLen, const int bLen);
 
 
@@ -11,39 +14,35 @@ void sortTwoArrays(int a[], const int b[], const int aLen, const int bLen);
  * Main program
  */
 int main() {
-    // Set up random number generator
-    srand(static_cast<unsigned int>(time(0)));
 
-    // Define the size of the arrays
-    const int size1 = 15 + rand() % 6;  // Random size between 15 and 20
-    const int size2 = 5 + rand() % 6;   // Random size between 5 and 10
+    srand(static_cast<unsigned int>(time(0)));                          // Set up random number generator
 
-    int array1[size1];
-    int array2[size2];
+    const int SIZE1 = 15 + rand() % 6;                                  // Random size between 15 and 20
+    const int SIZE2 = 5 + rand() % 6;                                   // Random size between 5 and 10
 
-    // Fill array2 with random integers (both positive and negative)
-    for (int i = 0; i < size2; i++) {
-        array2[i] = (rand() % 21 - 10); // Random number between -10 and 10
-    }
-    // Fill array1 with random integers and ensure it contains as many zeros as array2's size
-    int zerosToAdd = size2; // Number of zeros to add to array1
-    for (int i = 0; i < size1; i++) {
-        if (zerosToAdd > 0 && rand() % 2 == 0) {
+    int array1[SIZE1];
+    int array2[SIZE2];
+
+    for (int i = 0; i < SIZE2; i++) array2[i] = (rand() % 21 - 10);     // Fill array2 with integers between -10 and 10
+
+    int zerosToAdd = SIZE2;                                             // Number of zeros to add to array1
+    for (int i = 0; i < SIZE1; i++) {
+        if (zerosToAdd > 0 && rand() % 2 == 0) {                        // Either add a 0 to array1
             array1[i] = 0;
             zerosToAdd--;
-        } else array1[i] = (rand() % 21 - 10); // Random number between -10 and 10
+        } else array1[i] = (rand() % 21 - 10);                          // Or add random number between -10 and 10
     }
-    for (int i = size1 - zerosToAdd; i < size1; i++) array1[i] = 0; 
+    for (int i = SIZE1 - zerosToAdd; i < SIZE1; i++) array1[i] = 0;     // Fills the end of array1 with 0 if needed
 
 
     cout << "Array 1: ";
-    for (int i = 0; i < size1; i++) cout << array1[i] << ' ';
+    for (int i = 0; i < SIZE1; i++) cout << array1[i] << ' ';
     cout << "\nArray 2: ";
-    for (int i = 0; i < size2; i++) cout << array2[i] << ' ';
+    for (int i = 0; i < SIZE2; i++) cout << array2[i] << ' ';
     cout << endl;
 
 
-    sortTwoArrays(array1, array2, size1, size2); 
+    sortTwoArrays(array1, array2, SIZE1, SIZE2);                        // Combines and sorts array1 and 2 into the former array
 
     return 0;
 }
@@ -64,10 +63,10 @@ void sortTwoArrays(int a[], const int b[], const int aLen, const int bLen) {
             j++;
         }
     }
-    for(i = 2; i < aLen; i++) {
+    for(i = 1; i < aLen; i++) {
         value = a[i];
         j = i;
-        while (a[j-1] > value) {
+        while (a[j-1] > value && j > 0) {
             a[j] = a[j-1];
             j--;
         }
